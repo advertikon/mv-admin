b1ace590c748f86f94f363300ee2dbad1c94a983
Maxim Bogochunas <bogochunas@gmail.com>
2025-08-14 20:17:42 +0300

    fix: remove styles from companies page

--- src/sections/feed-app/companies/companies-list.section.tsx
+++ src/sections/feed-app/companies/companies-list.section.tsx
@@ -1,22 +1,16 @@
 import Paper from '@mui/material/Paper';
 import Box from '@mui/system/Box';
-import styled from '@mui/system/styled';
 import { CompaniesTable } from './companies-table';
-
-const ComponentBackground = styled(Paper)(({ theme }) => ({
-    borderColor: theme.palette.blue.main,
-    borderWidth: 1,
-    borderStyle: 'solid',
-    borderRadius: 10,
-    padding: 10,
-}));
 
 export function CompaniesListSection() {
     return (
         <Box>
-            <ComponentBackground elevation={24}>
+            <Paper
+                elevation={24}
+                style={{ borderColor: 'blue', borderWidth: 1, borderStyle: 'solid', borderRadius: 10, padding: 10 }}
+            >
                 <CompaniesTable />
-            </ComponentBackground>
+            </Paper>
         </Box>
     );
 }
